c93f10ef24010e8d896c6a7eaa6f79341adc6cf3
1cetsw <1cetsw@gmail.com>
2022-06-05 11:12:39 +0200

    push

--- seti.py
+++ seti.py
@@ -1,40 +1,60 @@
+# def decimal_to_binary(decimal_number):
+#     binary_numbers_list = []
+#     while decimal_number > 0:
+#         binary_number = decimal_number % 2
+#         binary_numbers_list.append(binary_number)
+#         #decimal_number = (decimal_number - binary_number) / 2
+#         decimal_number = decimal_number // 2
+#     #binary_numbers_list = list(reversed(binary_numbers_list))
+#     return binary_numbers_list[::-1]
+
 def decimal_to_binary(decimal_number):
-    binary_numbers_list = []
-    while decimal_number > 0:
-        binary_number = decimal_number % 2
-        binary_numbers_list.append(binary_number)
-        #decimal_number = (decimal_number - binary_number) / 2
-        decimal_number = decimal_number // 2
-    #binary_numbers_list = list(reversed(binary_numbers_list))
-    return binary_numbers_list[::-1]
+    decimal_to_base(decimal_number,2)
 
+# def binary_to_decimal(binary_digits):
+#     decimal = 0
+#     power = 0
+#     for x in binary_digits[::-1]:
+#         binary = x * (2**power)
+#         decimal += binary
+#         power += 1
 
-def binary_to_decimal(binary_digits):
-    decimal = 0
-    power = 0
-    for x in binary_digits[::-1]:
-        binary = x * (2**power)
-        decimal += binary
-        power += 1
-
-    #     for x in range(1, (len(binary_digits)+1)):
-    #
-    #         binary = binary_digits[-x] * (2 ** power)
-    #         decimal += binary
-    #         power += 1
-    return decimal
-    #         # 01010
-    #         #  0 8 0 2 0 = 10
+#     #     for x in range(1, (len(binary_digits)+1)):
+#     #
+#     #         binary = binary_digits[-x] * (2 ** power)
+#     #         decimal += binary
+#     #         power += 1
+#     return decimal
+#     #         # 01010
+#     #         #  0 8 0 2 0 = 10
 
 
 def decimal_to_base(decimal_number, destination_base):
     """Returns the digits in destination_base representation of the decimal number"""
-    pass
+    binary_numbers_list = []
+    while decimal_number > 0:
+        binary_number = decimal_number % destination_base
+        binary_numbers_list.append(binary_number)
+        
+        decimal_number = decimal_number // destination_base
+    #binary_numbers_list = list(reversed(binary_numbers_list))
+    return binary_numbers_list[::-1]
+
+    
+def binary_to_decimal(binary_digits):
+    base_to_decimal(binary_digits,2)
 
 
 def base_to_decimal(digits, original_base):
     """Returns the decimal (number) representation of an array of digits given in original_base"""
-    pass
+    decimal = 0
+    power = 0
+    for x in digits[::-1]:
+        binary = x * (original_base**power)
+        decimal += binary
+        power += 1
+
+    return decimal
 
 
 def digits_as_string(digits, base):
@@ -50,3 +70,4 @@ def convert_base(original_digits, original_base, destination_base):
 # print(lista)
 # list = [5, 4, 3, 2, 1]
 # print(list[::-1])
+print(decimal_to_base(20,2))
